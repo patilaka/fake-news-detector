461723db7bb1944772c0522f7c9716cafe5db79a
Aka Patil <pixellpenguiin@gmail.com>
2026-09-06 15:59:43 +0530

    Remove created_at field from PredictionHistory model
    Removed 'created_at' field from PredictionHistory model.

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,13 +1,12 @@
 from pydantic import BaseModel
-from datetime import datetime
 
 
 class PredictionHistory(BaseModel):
+
     id: int
     news_text: str
     prediction: str
     confidence: float
-    created_at: datetime
 
     class Config:
         from_attributes = True
